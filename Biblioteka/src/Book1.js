298f78bcafb43bd6e658d9ac59f952c49fb1dc61
Valeriia <139450358+Imachan1@users.noreply.github.com>
2025-10-02 14:46:57 +0200

    Add files via upload

---
+++ Biblioteka/src/Book1.js
@@ -0,0 +1,21 @@
+// const book = [
+//     {id: 0, title: "Hobbit", author: "J.R. Tolkien", pages: "300"},
+//     {id: 1, title: "Wladca pierscieni", author: "J.R. Tolkien", pages: "900"},
+//     {id: 2, title: "Wladca much", author: "W. Godling", pages: "250"}
+// ];
+
+// function Book(){
+//     return(
+//         <div className="App">
+//         {book.map((item,index)=>(
+//             <ol>
+//                 <h1>tytul: {item.title}</h1>
+//                 <h2>autor: {item.author}</h2>
+//                 <h4>ilosc stron: {item.pages}</h4>
+//             </ol>
+//         ))}
+//         </div>
+//     )
+// };
+
+// export default Book;
